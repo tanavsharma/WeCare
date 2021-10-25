7486cb4f29055d278d5238d221e8806485f8c75a
tanavsharma <sharma.tanav95@gmail.com>
2021-10-24 20:55:25 -0400

    trying to create the UI for Login and Signup Screen

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import Login from './screens/Login';
 
@@ -7,11 +6,4 @@ export default function App() {
   return <Login />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
